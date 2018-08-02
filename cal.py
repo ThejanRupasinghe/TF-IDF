4cbd2e26524b91af470cea9feaeca5836ca74966
ThejanRupasinghe <thejanrupasinghe.14@cse.mrt.ac.lk>
2018-08-02 18:47:27 +0530

    first commit

---
+++ cal.py
@@ -0,0 +1,33 @@
+from stop_words import get_stop_words
+from nltk.corpus import stopwords
+from nltk import sent_tokenize, word_tokenize, re
+import codecs
+import string
+
+stop_words = list(get_stop_words('en'))
+nltk_words = list(stopwords.words('english'))
+stop_words.extend(nltk_words)
+
+final_stop_words = list(set(stop_words))
+final_stop_words.extend([",", ".", "(", ")"])
+
+file = open('document1.txt', 'r')
+
+with open('document1.txt', 'rb') as f:
+    text = f.read()
+
+text = text.decode('utf-8', errors='ignore')
+text = str(text)
+
+sentences = sent_tokenize(text)
+words = word_tokenize(text.translate(string.punctuation))
+
+words = [w.lower() for w in words]
+
+words = [w for w in words if (w not in final_stop_words) and (not len(w) <= 2)]
+
+words = [w.replace(",", "") for w in words]
+
+# u.s.a drops
+words = [w for w in words if w.isalpha()]
+print(words)
